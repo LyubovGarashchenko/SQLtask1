9ce1f81fca485f88f6f9d100bed67a02f05b5992
Lyubov Garashchenko <lyubov-balabina@yandex.ru>
2024-04-23 14:24:32 +0300

    Database

---
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -0,0 +1,16 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.TypeOptions.text;
+
+public class DashboardPage {
+    private final SelenideElement heading = $("[data-test-id=dashboard]");
+    public DashboardPage() {
+        heading.shouldHave(Condition.text("Личный кабинет")).shouldBe(visible);
+    }
+
+}
---
+++ src/test/java/ru/netology/test/BankVerificationTest.java
@@ -0,0 +1,50 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.*;
+import ru.netology.data.DataHelper;
+import ru.netology.data.SQLHelper;
+import ru.netology.page.LoginPage;
+import static ru.netology.data.SQLHelper.cleanAuthCodes;
+import static ru.netology.data.SQLHelper.cleanDatabase;
+
+public class BankVerificationTest {
+    LoginPage loginPage;
+    @AfterEach
+    void tearDown(){
+        cleanAuthCodes();
+    }
+    @AfterAll
+    static void tearDawnAll(){
+        cleanDatabase();
+    }
+    @BeforeEach
+    void setUp(){
+        var loginPage = Selenide.open("http://localhost:9999", LoginPage.class);
+    }
+    @Test
+    @DisplayName("Should test successfully login and password from Database")
+    void shouldTestSuccessfullyLoginAndPasswordFromDatabase(){
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.visibleVerificationPage();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+    @Test
+    @DisplayName("Should test with error notification if user has random verification code")
+    void shouldTestWithErrorNotificationIfUserHasRandomVerificationCode(){
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.visibleVerificationPage();
+        var verificationCode = DataHelper.generateRandomVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+        verificationPage.setErrorNotification("Ошибка! \nНеверно указан код! Попробуйте ещё раз.");
+    }
+    @DisplayName("Should test with error notification if user is not exist in Database")
+    void shouldTestWithErrorNotificationIfUserIsNotExistInDatabase() {
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.validLogin(authInfo);
+        loginPage.setErrorNotification("Ошибка! \nНеверно указан логин или пароль");
+    }
+}
